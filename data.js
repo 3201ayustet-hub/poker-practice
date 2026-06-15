afe0d6e822223620e5657eac237bedddcbc68203
3201ayustet-hub <3201ayustet@gmail.com>
2026-06-15 20:58:26 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+// data.js
+// GitHub上でこのファイルを更新すると、検索条件・画像データが変わります。
+
+const OPTIONS = {
+  players: ["2max", "3max", "4max", "5max", "6max", "9max"],
+  stack: ["10bb", "15bb", "20bb", "25bb", "30bb", "40bb", "50bb", "100bb"],
+  position: ["UTG", "HJ", "CO", "BTN", "SB", "BB"],
+  action: ["Open", "Call", "3Bet", "4Bet", "All-in", "Fold"]
+};
+
+const IMAGE_DATA = [];
